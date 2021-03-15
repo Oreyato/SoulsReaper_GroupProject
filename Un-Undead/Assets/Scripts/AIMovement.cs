91b930a260a85591ea29d6622beebd0c314129eb
Tlouel <getrey.morgane@gmail.com>
2021-03-15 12:04:25 +0100

    In progress momo

---
+++ Un-Undead/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    public Transform goal;
+
+    void Awake() 
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+        
+    }
+
+
+}
