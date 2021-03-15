1500a6b567d99245edfa29396dec05ce78310180
Tlouel <getrey.morgane@gmail.com>
2021-03-15 14:35:50 +0100

    In progress momo

---
+++ SoulsReaperUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    void Update() //Pause the Game 
+    {
+        if (Input.GetKeyDown(KeyCode.P) && Time.timeScale != 0)
+        {
+            Time.timeScale = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
