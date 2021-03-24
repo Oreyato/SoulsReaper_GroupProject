fcdedc148ffa7c160ce4e501937b04ed88aac4f5
Oreyato <oreyato.pro@gmail.com>
2021-03-24 16:15:36 +0100

    Update ItemInfos.cs

--- SoulsReaperUnity/Assets/Scripts/Shop/ItemInfos.cs
+++ SoulsReaperUnity/Assets/Scripts/Shop/ItemInfos.cs
@@ -16,33 +16,3 @@ public class ItemInfos : MonoBehaviour
         
     }
 }
-/*
-using ARTFX.Collections;
-public class CharacterSheet 
-{
-    public string name = "Paul";
-    public string surname = "Cerceau";
-    public int age = 22;
-    public string job = "Programmer";
-
-    public List<Skills> skills = new List<Skills> 
-    {
-        //new Skills {Name = "", Rating = },
-        new Skills {Name = "Programming", Rating = 7},
-        new Skills {Name = "Traditional Drawing", Rating = 7},
-        new Skills {Name = "Digital Painting", Rating = 6},
-        //Softwares
-        new Skills {Name = "PhotoShop", Rating = 9},
-        new Skills {Name = "Krita", Rating = 7},
-        new Skills {Name = "Affinity", Rating = 1}, //What is it actually?
-        new Skills {Name = "3DS MAX", Rating = 5},
-        new Skills {Name = "Unity", Rating = 6},
-        //Others
-        new Skills {Name = "Creativity", Rating = 8},
-        new Skills {Name = "Multitasking", Rating = 8},
-        new Skills {Name = "Pedagogy", Rating = 8},
-        new Skills {Name = "Social", Rating = 6},
-        new Skills {Name = "Jokes", Rating = -10} //According to others :/
-    };
-}
-*/
