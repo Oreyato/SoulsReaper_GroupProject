38fe4321ab4a52d5178c0637069e955a23731fd0
MatthieuDubuisson <mdubuisson@artfx.fr>
2021-03-25 10:24:33 +0100

    deso Polo

--- SoulsReaperUnity/Assets/Scripts/GameManagement/GameManager.cs
+++ SoulsReaperUnity/Assets/Scripts/GameManagement/GameManager.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject PausePanel;
 
     void Update() //Pause the Game 
     {
         if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale != 0)
         {
-            Time.timeScale = 0;
+            PauseMenu();
         }
         else if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 0)
         {
-            Time.timeScale = 1;
+            ClosePauseMenu();
         }
+    }
+
+    public void PauseMenu()
+    {
+        Time.timeScale = 0;
+        SfxManager.sfxInstance.PauseMenuOpen.PlayOneShot(SfxManager.sfxInstance.PauseMenuOpenSnd);
+        PausePanel.SetActive(true);
+    }
+
+    public void ClosePauseMenu()
+    {
+        Time.timeScale = 1;
+        SfxManager.sfxInstance.PauseMenuClose.PlayOneShot(SfxManager.sfxInstance.PauseMenuCloseSnd);
+        PausePanel.SetActive(false);
     }
 }
