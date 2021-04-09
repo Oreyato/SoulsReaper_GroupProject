e3876960dd7e3fe2811e098ce667733c5ca42bca
Oreyato <oreyato.pro@gmail.com>
2021-04-09 19:27:49 +0200

    Updating ShopCanvas

--- SoulsReaperUnity/Assets/Scripts/Shop/ChosenObject.cs
+++ SoulsReaperUnity/Assets/Scripts/Shop/ChosenObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChosenPanel : MonoBehaviour
+public class ChosenObject : MonoBehaviour
 {
     private int buy = 0;
     private float souls = 0f;
